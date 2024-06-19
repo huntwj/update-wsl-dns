8ea09ac405bf9ba4300e63407fd6d69b1d9448cf
Wil Hunt <huntwj@gmail.com>
2024-06-19 16:38:43 -0700

    Add clap for cli args

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,19 @@
 use std::process::Command;
 
+use clap::Parser as _;
+use cli::Cli;
 use search::Search;
 
+mod cli;
 mod resolv;
 mod search;
 
 fn main() -> anyhow::Result<()> {
-    let command = "/mnt/c/Windows/System32/ipconfig.exe";
+    let cli_args = Cli::parse();
+
     let args = ["/all"];
 
-    let out = Command::new(command)
+    let out = Command::new(cli_args.command)
         .args(args)
         .output()
         .expect("Error running ipconfig.exe command.");
@@ -17,9 +21,7 @@ fn main() -> anyhow::Result<()> {
     let all_output = String::from_utf8(out.stdout)
         .expect("Could not create string stdout. Perhaps it was not UTF-8 encoded?");
 
-    let search = Search::from(all_output.as_str());
-
-    search.generate_resolv_conf(Box::new(std::io::stdout()))?;
+    Search::from(all_output.as_str()).generate_resolv_conf(Box::new(std::io::stdout()))?;
 
     Ok(())
 }
